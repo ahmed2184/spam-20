2de7c2a634f6f7fb25e6249895f725881c3ceddb
ahmed2184 <44103510+ahmed2184@users.noreply.github.com>
2018-11-22 23:04:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,6 +18,34 @@ channel.send(`sadfasdfasdfasdfasdfasdfasdfasdfasdfsadfasdfasdfasdf`);
 }, 25)
 })
 
+var prefix = "20";
+
+client.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+
+// -say
+
+  if (command === "say") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
+});
 
 
 
